7f7e2b9a7e0d13430518cdb3ec8aa4d58071cdcd
Zily13 <zily13@foxmail.com>
2020-06-08 02:01:32 +0800

    the second version of tested function 'get_id'

--- get_id.c
+++ get_id.c
@@ -4,7 +4,7 @@
 
 char** get_id(char * gb_filename){  //字符串gb_filename是等待被转化为FASTA格式的genbank文件的文件名
   int maxnum_id=500;  //从文件中提取到的id的最大数量，数值上应该和CDS_info结构体数量一致
-  FILE* genbank=fopen("gb_filename","r"); 
+  FILE* genbank=fopen(gb_filename,"r"); 
   char** protein_id=(char**)malloc(maxnum_id*sizeof(char*));  //protein_id是存储提取出的指向id的指针的存储空间的首地址
   char temp[50];  //temp是暂时存储protein_id的字符串
   char eachline[500];  //eachline存储fgets()函数得到的文件的一行
@@ -20,10 +20,10 @@ char** get_id(char * gb_filename){  //字符串gb_filename是等待被转化为F
     while(eachline[i]=='\t' || eachline[i]==' '){
       i++;
     }
-    if (eachline[i]=='\\' && eachline[i+1]=='p' && eachline[i+2]=='r' && eachline[i+3]=='o' && eachline[i+4]=='t' && eachline[i+5]=='e'){
+    if (eachline[i]=='/' && eachline[i+1]=='p' && eachline[i+2]=='r' && eachline[i+3]=='o' && eachline[i+4]=='t' && eachline[i+5]=='e'){
       i=i+13;
       while(eachline[i]!='\"'){
-	temp[j]=strand[i];
+	temp[j]=eachline[i];
 	i++;
 	j++;
       }
@@ -48,3 +48,4 @@ char** get_id(char * gb_filename){  //字符串gb_filename是等待被转化为F
   }
   return protein_id;
 }
+
